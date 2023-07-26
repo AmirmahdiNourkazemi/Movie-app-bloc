a980b49b5e1986ecb8de2dd82ae7ba7fe099112d
Amirmahdi1380 <nourkazemi80@gmail.com>
2023-07-26 15:58:10 +0330

    Revert "seprate main screen and home screen"
    This reverts commit 581e87ce2a394a707843c33a1a5bcd4d859fa696.

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,10 +1,7 @@
-import 'package:flashy_tab_bar2/flashy_tab_bar2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movie_app/bloc/home/home_bloc.dart';
 import 'package:movie_app/screens/home_screen/home_screen.dart';
-import 'package:movie_app/screens/search/search_scree.dart';
-import 'package:movie_app/screens/watch_list_screen/watch_list_screen.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -14,58 +11,13 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: ClipRRect(
-        borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(30),
-          topRight: Radius.circular(30),
-        ),
-        child: FlashyTabBar(
-          backgroundColor: const Color(0xff1F2722),
-          height: 55,
-          iconSize: 22,
-          selectedIndex: _selectedIndex,
-          showElevation: true,
-          onItemSelected: (index) => setState(() {
-            _selectedIndex = index;
-          }),
-          items: [
-            FlashyTabBarItem(
-              activeColor: Colors.white,
-              icon: const Icon(Icons.home),
-              title: const Text('Home'),
-            ),
-            FlashyTabBarItem(
-              activeColor: Colors.white,
-              icon: const Icon(Icons.search),
-              title: const Text('Search'),
-            ),
-            FlashyTabBarItem(
-              activeColor: Colors.white,
-              icon: const Icon(Icons.theaters),
-              title: const Text('watch list'),
-            ),
-          ],
-        ),
-      ),
-      body: IndexedStack(
-        index: _selectedIndex,
-        children: getScreen(),
+      body: BlocProvider(
+        create: (context) => HomeBloc(),
+        child: HomeScreen(),
       ),
     );
   }
-}
-
-List<Widget> getScreen() {
-  return <Widget>[
-    BlocProvider(
-      create: (context) => HomeBloc(),
-      child: const HomeScreen(),
-    ),
-    SearchScreen(),
-    WatchListScreen()
-  ];
 }
